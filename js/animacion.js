edc48d599778c6f145f9be7e1288178f666068cd
Mixerbc <90996295+Mixerbc@users.noreply.github.com>
2023-07-18 15:48:56 -0600

    "primero"

---
+++ js/animacion.js
@@ -0,0 +1,70 @@
+AOS.init({
+    duration: 1500,
+});
+    
+function Cambio(icono,color,logo){
+    document.querySelector(".circle").style.background = color;
+    document.querySelector(".starbucks").src = icono;
+    document.querySelector(".textbox span").style.color = color;
+    document.querySelector(".textbox a").style.background = color;
+    document.querySelector(".logo").src = logo;
+}
+
+document.querySelector(".thumb1").addEventListener("click",(e)=>{
+    e.preventDefault();
+    document.querySelector(".bg").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".raios").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".reward").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".overlay").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".overlay2").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".overlay3").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".overlay4").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".overlay5").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".overlay6").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    document.querySelector(".letras").style.color =  "#017143"
+    document.querySelector(".more::before").style.background = "linear-gradient( #017143, #099159, rgb(8, 217, 123))";
+    
+    
+ })
+ document.querySelector(".thumb2").addEventListener("click",(e)=>{
+    e.preventDefault();
+    document.querySelector(".bg").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".raios").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".reward").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".overlay").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".overlay2").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".overlay3").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".overlay4").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".overlay5").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".overlay6").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+    document.querySelector(".letras").style.color =  "#fdaeae"
+    document.querySelector(".more::before").style.background = "linear-gradient( #fdaeae, #f9bebe, #f9dada)";
+
+    
+    
+ })
+ document.querySelector(".thumb3").addEventListener("click",(e)=>{
+    e.preventDefault();
+    document.querySelector(".bg").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".raios").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".reward").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".overlay").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".overlay2").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".overlay3").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".overlay4").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".overlay5").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".overlay6").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+    document.querySelector(".letras").style.color =  "#ee3c83";
+    document.querySelector(".letras").style.color =  "#ee3c83";
+    document.querySelector(".more::before").style.background = "linear-gradient( #ee3c83, #e5679a, #f4a1c2)";
+
+    
+    
+    
+    
+    
+    
+ });
+
+ 
+ 
